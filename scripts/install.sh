2990aab84db85668da2627ec8b10d9fe6e119a0d
Keyi Zhang <keyi@stanford.edu>
2019-05-03 22:56:30 -0700

    compile garnet

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,14 +17,14 @@ pip install -r ${REQUIREMENTS}
 git clone https://github.com/StanfordAHA/garnet
 git clone --branch fix_abi https://github.com/StanfordAHA/Halide-to-Hardware
 
+# install Genesis
+git clone https://github.com/StanfordVLSI/Genesis2
+
 # download the prebuilt Halide library
 cd Halide-to-Hardware
 curl -s https://api.github.com/repos/StanfordAHA/Halide-to-Hardware/releases/latest?access_token=$GITHUB_TOKEN | grep browser_download_url | cut -d '"' -f 4 | wget -qi -
 tar zxvf halide_distrib.tgz
 ls distrib
 cd ../
-
-# install Genesis
-git clone https://github.com/StanfordVLSI/Genesis2.git
 
 date
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,12 +1,15 @@
-#!/bin/bash -e
+#!/bin/bash -xe
 
 export COREIR_DIR=/GarnetFlow/pycoreir/coreir-cpp
 export OUTPUT_REDIRECTION=""
 
 # add Genesis to the path
-export GENESIS_HOME=`pwd`/Genesis2/Genesis2Tools
+export GENESIS_HOME=/GarnetFlow/Genesis2/Genesis2Tools
 export PATH=$GENESIS_HOME/bin:$GENESIS_HOME/gui/bin:$PATH
 export PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$PERL5LIB
 
 # build CGRA
+make garnet_verilog
+
+# build apps
 make pointwise
