62e76108fd62fdc8a92080fc56bbdc562c2bd2f2
Keyi Zhang <keyi@stanford.edu>
2019-05-06 19:01:40 -0700

    fix everything... I hope

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,12 +4,8 @@ SOURCE="${BASH_SOURCE[0]}"
 REQUIREMENTS=`(dirname ${SOURCE})`/requirements.txt
 
 # build coreir here
-git clone https://github.com/Kuree/pycoreir.git
-git clone https://github.com/rdaly525/coreir pycoreir/coreir-cpp
-# build coreir normally
-cd pycoreir/coreir-cpp/build && cmake .. && make -j2 && cd /GarnetFlow
-
-# apply pycoreir patch so that it won't trying to install
+git clone --depth 1 https://github.com/leonardt/pycoreir.git
+git clone --depth 1 https://github.com/rdaly525/coreir pycoreir/coreir-cpp
 
 pip install -e pycoreir/
 
@@ -17,26 +13,26 @@ pip install -e pycoreir/
 pip install -r ${REQUIREMENTS}
 
 # re-compile again with the patch
-# current master branch
-# cd pycoreir/coreir-cpp && \
-#     git apply /GarnetFlow/patches/coreir_path_cxxopt && \
-#     cd build && \
-#     make -j2 && \
-#     cd /GarnetFlow
-
-# remove this after coreir bug is fixed
-# pip install --ignore-installed coreir
+# there is a bug in ABI with regex
+# see https://github.com/rdaly525/coreir/issues/737
+cd pycoreir/coreir-cpp && \
+    git apply /GarnetFlow/patches/coreir_path_cxxopt && \
+    cd build && \
+    make -j2 && \
+    cd /GarnetFlow
 
 # clone other repos
-git clone --branch simple_mapper https://github.com/StanfordAHA/garnet
+git clone --branch simple_mapper --depth 1 https://github.com/StanfordAHA/garnet
 git clone --depth 1 https://github.com/StanfordAHA/Halide-to-Hardware
 
 # install Genesis and apply patch
-git clone https://github.com/StanfordVLSI/Genesis2
+git clone --depth 1 https://github.com/StanfordVLSI/Genesis2
 rm -rf Genesis2/Genesis2Tools/PerlLibs/ExtrasForOldPerlDistributions/Compress
 
 # download the prebuilt Halide library
 cd Halide-to-Hardware
+# apply Halide patch
+git apply /GarnetFlow/patches/halide-hardware
 curl -s https://api.github.com/repos/StanfordAHA/Halide-to-Hardware/releases/latest?access_token=$GITHUB_TOKEN | grep browser_download_url | cut -d '"' -f 4 | wget -qi -
 tar zxvf halide_distrib.tgz
 ls distrib
