cc3502e7e4a8d37817fff9a13538744ff0a6c1df
Keyi Zhang <keyi@stanford.edu>
2019-05-07 11:29:49 -0700

    change the download script

--- scripts/install.sh
+++ scripts/install.sh
@@ -31,7 +31,7 @@ rm -rf Genesis2/Genesis2Tools/PerlLibs/ExtrasForOldPerlDistributions/Compress
 
 # download the prebuilt Halide library
 cd Halide-to-Hardware
-curl https://api.github.com/repos/StanfordAHA/Halide-to-Hardware/releases/latest?access_token=$GITHUB_TOKEN | grep browser_download_url | cut -d '"' -f 4 | wget -qi -
+curl -s -L https://github.com/StanfordAHA/Halide-to-Hardware/latest | grep "*.tgz" | cut -d \" -f 2 | xargs -I {} wget https://github.com"{}"
 tar zxvf halide_distrib.tgz
 ls distrib
 cd ../
