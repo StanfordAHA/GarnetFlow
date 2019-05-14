c3be362edec0ecacee90fd386d3c52a61916615f
Keyi Zhang <keyi@stanford.edu>
2019-05-14 09:46:04 -0700

    try out 10x10 on travis

--- scripts/run.sh
+++ scripts/run.sh
@@ -9,8 +9,8 @@ export GENESIS_HOME=/GarnetFlow/Genesis2/Genesis2Tools
 export PATH=$GENESIS_HOME/bin:$GENESIS_HOME/gui/bin:$PATH
 export PERL5LIB=$GENESIS_HOME/PerlLibs/ExtrasForOldPerlDistributions:$PERL5LIB
 
-export WIDTH=6
-export HEIGHT=6
+export WIDTH=10
+export HEIGHT=10
 
 # build CGRA
 make garnet_verilog
