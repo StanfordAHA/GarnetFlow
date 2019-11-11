c31f945057b0125ba571af6d118c365ebe2c3ca8
Dillon Huff <dillonhuff@gmail.com>
2019-11-10 17:08:44 -0800

    Updated ubuffer build tag

--- scripts/install.sh
+++ scripts/install.sh
@@ -43,7 +43,7 @@ cd Halide-to-Hardware
 # export RELEASE_ADDR=https://api.github.com/repos/StanfordAHA/Halide-to-Hardware/releases/latest
 # curl -X GET -u $GITHUB_TOKEN:x-oauth-basic ${RELEASE_ADDR} | grep browser_download_url | cut -d '"' -f 5 | wget -qi -
 #wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/v0.0.8/halide_distrib.tgz
-wget -q https://github.com/dillonhuff/Halide-to-Hardware/releases/download/test-ubuffer-2/halide_distrib.tgz
+wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/test-ubuffer-3/halide_distrib.tgz
 tar zxf halide_distrib.tgz
 ls distrib
 cd ../
