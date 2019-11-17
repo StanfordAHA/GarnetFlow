5ee7e556facfc6c068ba269870e805a69a02752e
Dillon Huff <dillonhuff@gmail.com>
2019-11-16 16:03:41 -0800

    Changes to scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,25 +3,37 @@
 SOURCE="${BASH_SOURCE[0]}"
 REQUIREMENTS=`(dirname ${SOURCE})`/requirements.txt
 
-# test of push ability
-# build coreir here
-#git clone --depth 1 --branch rm-ubuf https://github.com/rdaly525/coreir coreir
-git clone --depth 1 --branch ubuffer https://github.com/rdaly525/coreir coreir
-# there is a bug in ABI with regex
-# see https://github.com/rdaly525/coreir/issues/737
-#cd coreir && \
-    #git apply /GarnetFlow/patches/coreir_path_cxxopt && \
-    #cd ..
-cd coreir/build && cmake .. && make -j2 && cd ../../
+if [ ! -d "/usr/lib/llvm-7" ] then
+	apt-get install llvm-7
+	apt-get install clang-7
+else
+	echo "llvm already installed"
+fi
+
+# If coreir has not already been built
+if [ ! -d "coreir" ]; then
+	# test of push ability
+	# build coreir here
+	#git clone --depth 1 --branch rm-ubuf https://github.com/rdaly525/coreir coreir
+	git clone --depth 1 --branch ubuffer https://github.com/rdaly525/coreir coreir
+	# there is a bug in ABI with regex
+	# see https://github.com/rdaly525/coreir/issues/737
+	#cd coreir && \
+	#git apply /GarnetFlow/patches/coreir_path_cxxopt && \
+	#cd ..
+	cd coreir/build && cmake .. && make -j4 && cd ../../
+else
+	echo "Coreir already built"
+fi
 
 # pin pycoreir version since the latest one doesn't work
-pip install --ignore-installed coreir==2.0.19
+pip3 install --ignore-installed coreir==2.0.19
 
 # install this last since we already have a coreir built
-python python_repo.py
+python3 python_repo.py
 
 # install genesis2
-pip install genesis2
+pip3 install genesis2
 
 # clone garnet
 git clone --depth 1 https://github.com/StanfordAHA/garnet
@@ -39,15 +51,24 @@ git clone --branch hls_codegen_with_loops_hwbuffer_jeff_merge --depth 1 https://
 
 # download the prebuilt Halide library
 cd Halide-to-Hardware
+export CLANG=/usr/lib/llvm-7/bin/clang
+export LLVM_CONFIG=/usr/lib/llvm-7/bin/llvm-config
+export CLANG_OK=y
+
+make -j
+make distrib
+
+
+
 # I give up. someone else can try it.
 # export RELEASE_ADDR=https://api.github.com/repos/StanfordAHA/Halide-to-Hardware/releases/latest
 # curl -X GET -u $GITHUB_TOKEN:x-oauth-basic ${RELEASE_ADDR} | grep browser_download_url | cut -d '"' -f 5 | wget -qi -
 #wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/v0.0.8/halide_distrib.tgz
 #wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/test-buffer-4/halide_distrib.tgz
 #wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/ubuffer-abi-fix/halide_distrib.tgz
-wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/ubuffer-using-llvm-7/halide_distrib.tgz
-tar zxf halide_distrib.tgz
-ls distrib
+# wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/ubuffer-using-llvm-7/halide_distrib.tgz
+#tar zxf halide_distrib.tgz
+#ls distrib
 cd ../
 
 date
