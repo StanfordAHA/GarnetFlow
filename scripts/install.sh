b318ca49587208d74ad755647851e8bba9399467
Dillon Huff <dillonhuff@gmail.com>
2019-11-16 17:39:50 -0800

    Added comment

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 
+# Make sure we are using python3 for everything
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 source install_halide_deps.sh
 source install_and_build_halide.sh
