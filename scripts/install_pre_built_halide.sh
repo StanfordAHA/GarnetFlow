436f33bed40af2dab618d7e2e1da48bf6ebf0c92
Dillon Huff <dillonhuff@gmail.com>
2019-11-16 23:16:46 -0800

    Now using pre-built halide to hardware

--- scripts/install_pre_built_halide.sh
+++ scripts/install_pre_built_halide.sh
@@ -4,7 +4,7 @@ git clone --branch hls_hwbuffer_codegen_garnetflow_updates --depth 1 https://git
 
 cd Halide-to-Hardware
 
-wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/test-ubuffer-4/halide_distrib.tgz
+wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/travis-docker-integration-3/halide_distrib.tgz
 tar zxf halide_distrib.tgz
 cd ../
 
