fe62b9471477503085a4f88bc15b73ef7a7f23f7
Dillon Huff <dillonhuff@gmail.com>
2019-11-16 21:46:12 -0800

    Modified garnetflow install.sh to use pre-built halide

---
+++ scripts/install_pre_built_halide.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+git clone --branch hls_hwbuffer_codegen_garnetflow_updates --depth 1 https://github.com/StanfordAHA/Halide-to-Hardware
+
+cd Halide-to-Hardware
+
+wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/test-ubuffer-4/halide_distrib.tgz
+tar zxf halide_distrib.tgz
+cd ../
+
+date
