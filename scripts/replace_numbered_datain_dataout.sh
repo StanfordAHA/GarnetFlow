7fc2104489ec8a2a1b228ecfbf02d24a8658b05f
Dillon Huff <dillonhuff@gmail.com>
2019-11-18 14:59:21 -0800

    Now the coreir source file read in map_app has a connection on self.out that does not appear anywhere else?

--- scripts/replace_numbered_datain_dataout.sh
+++ scripts/replace_numbered_datain_dataout.sh
@@ -1,2 +1,2 @@
 awk '{gsub("datain0", "datain.0", $0); print}' $1 > /tmp/dout_tmp.json
-awk '{gsub("dataout0", "dataout", $0); print}' /tmp/dout_tmp.json
+awk '{gsub("dataout0", "dataout.0", $0); print}' /tmp/dout_tmp.json
