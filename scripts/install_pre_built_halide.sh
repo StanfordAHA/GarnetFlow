a0f68bc54d0d57cfb1394c3fd11fc0efd91a2acc
Dillon Huff <dillonhuff@gmail.com>
2019-11-18 00:50:58 -0800

    Using no pre built code

--- scripts/install_pre_built_halide.sh
+++ scripts/install_pre_built_halide.sh
@@ -4,7 +4,8 @@ git clone --branch hls_hwbuffer_codegen_garnetflow_updates --depth 1 https://git
 
 cd Halide-to-Hardware
 
-wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/travis-docker-integration-3/halide_distrib.tgz
+#wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/travis-docker-integration-3/halide_distrib.tgz
+wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/linebuffer-codegen-for-garnetflow-no-wire/halide_distrib.tgz
 tar zxf halide_distrib.tgz
 cd ../
 
