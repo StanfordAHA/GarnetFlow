a7e32544bc0536c3738a7635ddc6d1a56e5260ec
Dillon Huff <dillonhuff@gmail.com>
2019-11-19 19:00:55 -0800

    Conv now runs

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@ update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 # Install dependencies
 source install_halide_deps.sh
 source install_garnet.sh
-#source install_pre_built_halide.sh
-source install_and_build_halide.sh StanfordAHA/Halide-to-Hardware hls_hwbuffer_codegen_garnetflow_updates
+source install_pre_built_halide.sh
+#source install_and_build_halide.sh StanfordAHA/Halide-to-Hardware hls_hwbuffer_codegen_garnetflow_updates
 
 date
--- scripts/install_pre_built_halide.sh
+++ scripts/install_pre_built_halide.sh
@@ -5,7 +5,8 @@ git clone --branch hls_hwbuffer_codegen_garnetflow_updates --depth 1 https://git
 cd Halide-to-Hardware
 
 #wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/travis-docker-integration-3/halide_distrib.tgz
-wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/ubuffer-garnetflow-abs-support/halide_distrib.tgz
+#wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/ubuffer-garnetflow-abs-support/halide_distrib.tgz
+wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/selective-inlining/halide_distrib.tgz
 tar zxf halide_distrib.tgz
 cd ../
 
