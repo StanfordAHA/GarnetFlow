f450f34177e69467d6cd0124d23e56cf957d9914
Dillon Huff <dillonhuff@gmail.com>
2019-11-21 15:59:29 -0800

    cascade as single conv matches cpu output locally on Dillons machine

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 # Make sure we are using python3 for everything
 update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 
-# Install dependencies
+# Install dependencies use latest H2H?
 source install_halide_deps.sh
 source install_garnet.sh
 source install_pre_built_halide.sh
