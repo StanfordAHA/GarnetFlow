2c60bfdb039d36025a1b613c375af5d24211f869
Dillon Huff <dillonhuff@gmail.com>
2019-11-20 22:36:56 -0800

    Modulus issues fixed in local tests

--- scripts/install_pre_built_halide.sh
+++ scripts/install_pre_built_halide.sh
@@ -6,7 +6,8 @@ cd Halide-to-Hardware
 #wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/travis-docker-integration-3/halide_distrib.tgz
 #wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/ubuffer-garnetflow-abs-support/halide_distrib.tgz
 #wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/selective-inlining/halide_distrib.tgz
-wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/memory-flatten-fix/halide_distrib.tgz
+#wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/memory-flatten-fix/halide_distrib.tgz
+wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/sign-mod-fix/halide_distrib.tgz
 tar zxf halide_distrib.tgz
 cd ../
 
