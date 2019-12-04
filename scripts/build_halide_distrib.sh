9a5d0de5827004365e23a741529cc13364b130b1
Dillon Huff <dillonhuff@gmail.com>
2019-12-04 11:28:04 -0800

    Removed some now unneeded script arguments

--- scripts/build_halide_distrib.sh
+++ scripts/build_halide_distrib.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
 
 source install_halide_deps.sh
-source install_and_build_halide.sh $1 $2
+source install_and_build_halide.sh
+#source install_and_build_halide.sh $1 $2
 cd Halide-to-Hardware
 rm -f distrib/halide.tgz
 rm -f distrib/libHalide.so
