2e476e9ca75a7cf17c52bec8b4ee464738dd5deb
Dillon Huff <dillonhuff@gmail.com>
2019-12-04 11:21:16 -0800

    Changed install and build halide to just build halide

--- scripts/install_and_build_halide.sh
+++ scripts/install_and_build_halide.sh
@@ -13,9 +13,9 @@ export LLVM_CONFIG=/usr/lib/llvm-7/bin/llvm-config
 export CLANG_OK=y
 
 # Get Halide
-git clone --branch hls_hwbuffer_codegen_garnetflow_updates --depth 1 https://github.com/$1
+#git clone --branch hls_hwbuffer_codegen_garnetflow_updates --depth 1 https://github.com/$1
 cd Halide-to-Hardware
-git checkout -qf $2
+#git checkout -qf $2
 
 make -j4
 make distrib
