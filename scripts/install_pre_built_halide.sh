030cc64cddaac64d76ef9062356ab312ef08e92e
Dillon Huff <dillonhuff@gmail.com>
2019-12-05 10:25:23 -0800

    Using Halide-to-Hardware master

--- scripts/install_pre_built_halide.sh
+++ scripts/install_pre_built_halide.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-git clone --branch hls_hwbuffer_codegen_garnetflow_updates --depth 1 https://github.com/StanfordAHA/Halide-to-Hardware
+git clone --depth 1 https://github.com/StanfordAHA/Halide-to-Hardware
 cd Halide-to-Hardware
 
 wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/cascade-cast-issue-resolved/halide_distrib.tgz
