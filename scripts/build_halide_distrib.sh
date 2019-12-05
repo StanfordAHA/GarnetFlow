7d4951f20eae7de84d98c4f67a09d8782b15c53f
Dillon Huff <dillonhuff@gmail.com>
2019-12-05 13:54:13 -0800

    Removed some comments

--- scripts/build_halide_distrib.sh
+++ scripts/build_halide_distrib.sh
@@ -2,7 +2,6 @@
 
 source install_halide_deps.sh
 source install_and_build_halide.sh
-#source install_and_build_halide.sh $1 $2
 cd Halide-to-Hardware
 rm -f distrib/halide.tgz
 rm -f distrib/libHalide.so
