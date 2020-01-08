9c109b97c76a84b7cf3c531f9ca746dadc0b5482
Dillon Huff <dillonhuff@gmail.com>
2020-01-08 11:50:02 -0800

    Updated to do auto-builds for Halide-to-Hardware pull requests

--- scripts/install_halide_deps.sh
+++ scripts/install_halide_deps.sh
@@ -21,9 +21,11 @@ fi
 
 # create the lake repo
 export COREIR_DIR=/GarnetFlow/scripts/coreir # lake needs to know where coreir is
+if [ ! -d "BufferMapping" ]; then
 git clone --branch master --depth 1 https://github.com/joyliu37/BufferMapping
 cd BufferMapping/cfunc
 make lib
 cd ../../
+fi
 
 date
