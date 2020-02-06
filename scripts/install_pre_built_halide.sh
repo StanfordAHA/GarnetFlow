084f81a734bb0b9ef5aefcf714e8db2b77118b5d
rsetaluri <rsetaluri@users.noreply.github.com>
2020-02-06 11:50:01 -0800

    Install older version of Halide-to-Hardware (5ed8864)

--- scripts/install_pre_built_halide.sh
+++ scripts/install_pre_built_halide.sh
@@ -3,6 +3,8 @@
 if [[ -z $Halide_to_Hardware ]]; then
   git clone --depth 1 https://github.com/StanfordAHA/Halide-to-Hardware
   cd Halide-to-Hardware
+  
+  git checkout 5ed8864604e381e661dc4dd37a16bd9dff0ee8e0
 
   wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/cascade-cast-issue-resolved/halide_distrib.tgz
   tar zxf halide_distrib.tgz
