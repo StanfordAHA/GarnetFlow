c8c3dc4fd45bef9021c0be00207b4b51aceeee5b
Keyi Zhang <keyi@stanford.edu>
2020-02-06 16:21:08 -0800

    use newest halide release

--- scripts/install_pre_built_halide.sh
+++ scripts/install_pre_built_halide.sh
@@ -4,7 +4,7 @@ if [[ -z $Halide_to_Hardware ]]; then
   git clone --depth 1 https://github.com/StanfordAHA/Halide-to-Hardware
   cd Halide-to-Hardware
 
-  wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/cascade-cast-issue-resolved/halide_distrib.tgz
+  wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/lakelib/halide_distrib.tgz
   tar zxf halide_distrib.tgz
 else
   git clone https://github.com/StanfordAHA/Halide-to-Hardware
