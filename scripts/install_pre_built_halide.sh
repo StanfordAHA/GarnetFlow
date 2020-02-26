c3c9f08e8b0873a8ca794c9bb08e44861bcb610a
Keyi Zhang <keyi@stanford.edu>
2020-02-25 21:25:38 -0800

    build halide from daily run

--- scripts/install_pre_built_halide.sh
+++ scripts/install_pre_built_halide.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -xe
 
-if [[ -z $Halide_to_Hardware ]]; then
+if [[ -z $PR ]]; then
+  git clone https://github.com/StanfordAHA/Halide-to-Hardware
+  ./install_and_build_halide.sh
+else
   git clone --depth 1 https://github.com/StanfordAHA/Halide-to-Hardware
   cd Halide-to-Hardware
 
   wget -q https://github.com/StanfordAHA/Halide-to-Hardware/releases/download/lakelib/halide_distrib.tgz
   tar zxf halide_distrib.tgz
-else
-  git clone https://github.com/StanfordAHA/Halide-to-Hardware
-  ./install_and_build_halide.sh
 fi
 
 cd ../
